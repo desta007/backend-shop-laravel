99c6269ab891c5922dd7145daecc066e1023e799
Desta <detajuventini@gmail.com>
2024-09-19 14:09:07 +0700

    feat: admin and restful API

---
+++ routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login'])->name('api.customer.login');
+Route::post('/register', [AuthController::class, 'register'])->name('api.customer.register');
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/user', [AuthController::class, 'getUser'])->name('api.customer.user');
+    /**
+     * Router Order
+     */
+    Route::get('/order', [OrderController::class, 'index'])->name('api.order.index');
+    Route::get('/order/{snap_token?}', [OrderController::class, 'show'])->name('api.order.show');
+
+    /**
+     * Route API Cart
+     */
+    Route::get('/cart', [CartController::class, 'index'])->name('customer.cart.index');
+    Route::post('/cart', [CartController::class, 'store'])->name('customer.cart.store');
+    Route::get('/cart/total', [CartController::class, 'getCartTotal'])->name('customer.cart.total');
+    Route::get('/cart/totalWeight', [CartController::class, 'getCartTotalWeight'])->name('customer.cart.getCartTotalWeight');
+    Route::post('/cart/remove', [CartController::class, 'removeCart'])->name('customer.cart.remove');
+    Route::post('/cart/removeAll', [CartController::class, 'removeAllCart'])->name('customer.cart.removeAll');
+});
+
+/**
+ * Route API Category
+ */
+Route::get('/categories', [CategoryController::class, 'index'])->name('customer.category.index');
+Route::get('/category/{slug?}', [CategoryController::class, 'show'])->name('customer.category.show');
+Route::get('/categoryHeader', [CategoryController::class, 'categoryHeader'])->name('customer.category.categoryHeader');
+
+/**
+ * Route API Product
+ */
+Route::get('/products', [ProductController::class, 'index'])->name('customer.product.index');
+Route::get('/product/{slug?}', [ProductController::class, 'show'])->name('customer.product.show');
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CustomerController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\SliderController;
+use App\Http\Controllers\Admin\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+/**
+ * route for admin
+ */
+
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+
+    //group route with middleware "auth"
+    Route::group(['middleware' => 'auth'], function () {
+
+        //route dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+        //route category
+        Route::resource('/category', CategoryController::class, ['as' => 'admin']);
+        //route product
+        Route::resource('/product', ProductController::class, ['as' => 'admin']);
+        //route order
+        Route::resource('/order', OrderController::class, ['except' => ['create', 'store', 'edit', 'update', 'destroy'], 'as' => 'admin']);
+        //route customer
+        Route::get('/customer', [CustomerController::class, 'index'])->name('admin.customer.index');
+        //route slider
+        Route::resource('/slider', SliderController::class, ['except' => ['show', 'create', 'edit', 'update'], 'as' => 'admin']);
+        //profile
+        Route::get('/profile', [ProfileController::class, 'index'])->name('admin.profile.index');
+        //route user
+        Route::resource('/user', UserController::class, ['except' => ['show'], 'as' => 'admin']);
+    });
+});
